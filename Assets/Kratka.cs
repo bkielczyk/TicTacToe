ae328d90bdf0c07f34a82e179204f262822503d6
bkielczyk255@technikumkreatywne.pl <bkielczyk255@technikumkreatywne.pl>
2021-09-23 16:59:23 +0200

    kółko zmienia się na krzyżyk po kliknięciu

--- Assets/Kratka.cs
+++ Assets/Kratka.cs
@@ -2,10 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Wybrane
+{
+    KRZYZYK,
+    KOLKO
+}
+
 public class Kratka : MonoBehaviour
 {
+    public Sprite kolko, krzyzyk;
+    Wybrane aktualnyWybor = Wybrane.KOLKO;
+
+    SpriteRenderer sr;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
     private void OnMouseDown()
     {
-        Debug.Log("Kratka KLIKnieta");
+        if (aktualnyWybor == Wybrane.KOLKO)
+        {
+            sr.sprite = krzyzyk;
+            aktualnyWybor = Wybrane.KRZYZYK;
+        }
+        else 
+        {
+            sr.sprite = kolko;
+            aktualnyWybor = Wybrane.KOLKO;
+        }
     }
 }
