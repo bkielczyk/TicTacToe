b5140d91614a5546a2d0b66abc15715544d5f7fd
bkielczyk255@technikumkreatywne.pl <bkielczyk255@technikumkreatywne.pl>
2021-09-23 15:23:01 +0200

    moje okienko

---
+++ Assets/Editor/MyWindow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MyWindow : EditorWindow
+{
+    [MenuItem("Window/Moje okienko")]
+    public static void OpenWindow()
+    {
+        GetWindow<MyWindow>();
+    }
+}
